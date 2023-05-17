578cb23da9ecd31456f14b994e10fdf911336b49
Luke Waldron <lukewaldron87@gmail.com>
2023-05-17 11:46:02 +0200

    added FeignClient to call currency-exchange

--- src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionController.java
+++ src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -1,5 +1,6 @@
 package com.in28minutes.microservices.currencyconversionservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +13,17 @@ import java.util.HashMap;
 @RestController
 public class CurrencyConversionController {
 
+    @Autowired
+    CurrencyExchangeProxy proxy;
+
+    /**
+     * Statically calling currency-exchange
+     *
+     * @param from
+     * @param to
+     * @param quantity
+     * @return
+     */
     @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversion calculateCurrencyConversion(@PathVariable String from,
                                                           @PathVariable String to,
@@ -33,6 +45,30 @@ public class CurrencyConversionController {
                 quantity,
                 currencyConversion.getConversionMultiple(),
                 quantity.multiply(currencyConversion.getConversionMultiple()),
-                currencyConversion.getEnvironment());
+                currencyConversion.getEnvironment() + " REST template");
+    }
+
+    /**
+     * Using a FeignClient to call currency-exchange
+     *
+     * @param from
+     * @param to
+     * @param quantity
+     * @return
+     */
+    @GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion calculateCurrencyConversionFeign(@PathVariable String from,
+                                                              @PathVariable String to,
+                                                              @PathVariable BigDecimal quantity){
+
+        CurrencyConversion currencyConversion = proxy.retrieveExchangeValue(from, to);
+
+        return new CurrencyConversion(10001L,
+                from,
+                to,
+                quantity,
+                currencyConversion.getConversionMultiple(),
+                quantity.multiply(currencyConversion.getConversionMultiple()),
+                currencyConversion.getEnvironment() + " feign");
     }
 }
